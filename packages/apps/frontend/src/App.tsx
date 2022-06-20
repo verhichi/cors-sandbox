d901e28eb612565570abacc47c7b6c8950a1847c
Daichi Nishida <verhichi@hotmail.com>
2022-06-21 00:06:11 +0900

    feat: add abstract

--- packages/apps/frontend/src/App.tsx
+++ packages/apps/frontend/src/App.tsx
@@ -1,26 +1,49 @@
 import axios from 'axios'
 import { useState } from 'react'
-import { Button, Space } from 'antd'
+import { DEFAULT_REQUEST_URL } from '@/constants'
+import { Input, Space, Typography } from 'antd'
+
+const { Search } = Input
+const { Paragraph, Text, Title } = Typography
 
 export const App = () => {
   const [loading, setLoading] = useState(false)
+  const [isNetworkError, setIsNetworkError] = useState(false)
 
-  const handleClickButton = async () => {
+  const handleClickButton = async (value: string) => {
     setLoading(true)
+    setIsNetworkError(false)
+
     try {
-      await axios.get('http://localhost:8080/api')
+      await axios.get(value || DEFAULT_REQUEST_URL)
     } catch (e) {
       console.log(e)
+      setIsNetworkError(true)
     } finally {
       setLoading(false)
     }
   }
 
   return (
-    <Space>
-      <Button type="primary" loading={loading} onClick={handleClickButton}>
-        Send Request!
-      </Button>
+    <Space direction="vertical">
+      <Space direction="vertical">
+        <Title level={1}>What is this?</Title>
+        <Paragraph>
+          This app helps you understand what is causing the CORS error by visualizing the
+          frontend request as well as the backend server setting and highlighting the key
+          factors that lead to the CORS error.
+        </Paragraph>
+      </Space>
+      <Space>
+        <Search
+          placeholder={DEFAULT_REQUEST_URL}
+          enterButton="Send Request"
+          onSearch={handleClickButton}
+          size="large"
+          loading={loading}
+        />
+        {isNetworkError && <Text type="danger">Network Error!</Text>}
+      </Space>
     </Space>
   )
 }
---
+++ packages/apps/frontend/src/constants/index.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_REQUEST_URL = 'http://localhost:8080/api'
