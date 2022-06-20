1316d4a48960a1c5bc23bead7710da396b09dad0
Daichi Nishida <verhichi@hotmail.com>
2022-06-21 01:18:01 +0900

    add all methods to backend

--- packages/apps/backend/src/app.controller.ts
+++ packages/apps/backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Delete, Put, Head, Options, Patch } from '@nestjs/common'
 import { AppService } from './app.service'
 
 @Controller('api')
@@ -7,6 +7,36 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Post()
+  postHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Delete()
+  deleteHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Put()
+  putHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Patch()
+  patchHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Head()
+  headHello(): string {
+    return this.appService.getHello()
+  }
+
+  @Options()
+  optionsHello(): string {
     return this.appService.getHello()
   }
 }
