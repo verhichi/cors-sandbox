da94788171e321dea1fdb3657426705ae316707a
Daichi Nishida <verhichi@hotmail.com>
2022-06-21 01:18:22 +0900

    feat: add method selection

--- packages/apps/frontend/src/App.tsx
+++ packages/apps/frontend/src/App.tsx
@@ -1,21 +1,28 @@
-import axios from 'axios'
+import axios, { Method } from 'axios'
 import { useState } from 'react'
-import { DEFAULT_REQUEST_URL } from '@/constants'
-import { Input, Space, Typography } from 'antd'
+import { DEFAULT_REQUEST_URL, METHODS } from '@/constants'
+import { Input, Space, Typography, Select, Table, Tag } from 'antd'
 
-const { Search } = Input
+const { Option } = Select
+const { Search, TextArea } = Input
 const { Paragraph, Text, Title } = Typography
 
 export const App = () => {
   const [loading, setLoading] = useState(false)
   const [isNetworkError, setIsNetworkError] = useState(false)
+  const [method, setMethod] = useState<Method>(METHODS[0])
+
+  const handleChangeMethod = (value: Method) => setMethod(value)
 
   const handleClickButton = async (value: string) => {
     setLoading(true)
     setIsNetworkError(false)
 
     try {
-      await axios.get(value || DEFAULT_REQUEST_URL)
+      await axios.request({
+        method,
+        url: value || DEFAULT_REQUEST_URL,
+      })
     } catch (e) {
       console.log(e)
       setIsNetworkError(true)
@@ -34,15 +41,36 @@ export const App = () => {
           factors that lead to the CORS error.
         </Paragraph>
       </Space>
-      <Space>
+      <hr />
+      <Space direction="vertical">
+        <Title level={1}>Try sending a request!</Title>
         <Search
           placeholder={DEFAULT_REQUEST_URL}
           enterButton="Send Request"
           onSearch={handleClickButton}
           size="large"
           loading={loading}
+          addonBefore={
+            <Select<Method> defaultValue={METHODS[0]} onChange={handleChangeMethod}>
+              {METHODS.map((method) => (
+                <Option key={method} value={method}>
+                  {method}
+                </Option>
+              ))}
+            </Select>
+          }
         />
         {isNetworkError && <Text type="danger">Network Error!</Text>}
+      </Space>
+      <Space>
+        <div className="left box">frontend</div>
+        <div className="middle">→→→→→→→</div>
+        <div className="right box">backend</div>
+      </Space>
+      <Space>
+        <TextArea />
+        <div className="middle">→→→→→→→</div>
+        <TextArea />
       </Space>
     </Space>
   )
--- packages/apps/frontend/src/constants/index.ts
+++ packages/apps/frontend/src/constants/index.ts
@@ -1,1 +1,12 @@
+import { Method } from 'axios'
+
 export const DEFAULT_REQUEST_URL = 'http://localhost:8080/api'
+export const METHODS: Method[] = [
+  'GET',
+  'DELETE',
+  'HEAD',
+  'OPTIONS',
+  'POST',
+  'PUT',
+  'PATCH',
+]
