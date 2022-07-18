3f98c74955c8d0c830b3e0cb098f4a32255c18a0
Daichi Nishida <verhichi@hotmail.com>
2022-07-18 21:02:57 +0900

    chore/front: separate TopRegion component

---
+++ packages/apps/frontend/src/App/components/TopRegion/TopRegion.tsx
@@ -0,0 +1,13 @@
+import { Space, Typography } from 'antd'
+const { Paragraph, Title } = Typography
+
+export const TopRegion = () => (
+  <Space direction="vertical">
+    <Title level={1}>What is this?</Title>
+    <Paragraph>
+      This app helps you understand what is causing the CORS error by visualizing the
+      frontend request as well as the backend server setting and highlighting the key
+      factors that lead to the CORS error.
+    </Paragraph>
+  </Space>
+)
